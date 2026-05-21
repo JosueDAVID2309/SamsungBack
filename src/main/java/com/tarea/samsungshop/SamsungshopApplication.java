f9a8ae83899d766eb709a1a96b3c0eab8c5d1e50
JosueDAVID2309 <josue.ataulluco.david@gmail.com>
2026-05-21 18:31:49 -0500

    Backend base

---
+++ src/main/java/com/tarea/samsungshop/SamsungshopApplication.java
@@ -0,0 +1,13 @@
+package com.tarea.samsungshop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SamsungshopApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SamsungshopApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/tarea/samsungshop/SamsungshopApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tarea.samsungshop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SamsungshopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
