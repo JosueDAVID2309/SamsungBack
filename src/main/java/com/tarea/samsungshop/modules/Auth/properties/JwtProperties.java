b3f416be6d43295e0362d01adf6299eb0168f87a
Josue <josue.ataulluco.david@gmail.com>
2026-05-26 13:22:01 -0500

    Auth module inicializado

---
+++ src/main/java/com/tarea/samsungshop/modules/Auth/properties/JwtProperties.java
@@ -0,0 +1,24 @@
+package com.tarea.samsungshop.modules.Auth.properties;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.stereotype.Component;
+
+@Component
+@ConfigurationProperties(prefix = "jwt")
+public class JwtProperties {
+    private String secret;
+    private long accessTokenExpiration;
+    private long refreshTokenExpiration;
+
+    public String getSecret() {
+        return secret;
+    }
+
+    public long getAccessTokenExpiration() {
+        return accessTokenExpiration;
+    }
+
+    public long getRefreshTokenExpiration() {
+        return refreshTokenExpiration;
+    }
+}
---
+++ src/main/java/com/tarea/samsungshop/modules/Auth/utils/TokenUtil.java
@@ -0,0 +1,34 @@
+package com.tarea.samsungshop.modules.Auth.utils;
+import java.util.Date;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import com.tarea.samsungshop.modules.Auth.properties.JwtProperties;
+
+import io.jsonwebtoken.Jwts;
+import io.jsonwebtoken.SignatureAlgorithm;
+
+
+public class TokenUtil {
+
+    @Autowired
+    JwtProperties jwtProperties;
+
+    public String generateAccessToken(String correo){
+        return Jwts.builder()
+            .setSubject(correo)
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + jwtProperties.getAccessTokenExpiration()))
+            .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecret())
+            .compact();
+    }
+
+    public String generateRefreshToken(String correo){
+        return Jwts.builder()
+            .setSubject(correo)
+            .setIssuedAt(new Date())
+            .setExpiration(new Date(System.currentTimeMillis() + jwtProperties.getRefreshTokenExpiration()))
+            .signWith(SignatureAlgorithm.HS256, jwtProperties.getSecret())
+            .compact();
+    }
+}
